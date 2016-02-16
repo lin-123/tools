07662fce87a39e46d11c4161b2c80140f2b0b86d
侯林江 <linjiang.hou@baidao.com>
2016-02-16 11:06:36 +0800

    change file: es6demo

--- es6demo/lib/babel-cli.js
+++ es6demo/lib/babel-cli.js
@@ -1,8 +1,6 @@
 'use strict';
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
-  return typeof obj;
-} : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
 var arr = [1, 2, 3];
 arr.map(function (item) {
