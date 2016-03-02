d0912ab5e36bee1021de7981125e959de143ee56
侯林江 <linjiang.hou@baidao.com>
2016-03-02 17:32:06 +0800

    client+server: update server.js

--- client+server/server.js
+++ client+server/server.js
@@ -7,13 +7,19 @@ http.createServer(function(req, res){
   console.log('request: ', req.url)
   url = req.url
 
+  // 路由到下层
   if(typeof(router[url]) === 'function')
     return router[url](req, res)
 
+  // 访问文件
   if(url == '/') url = '/index.html'
   file_path = path.join(__dirname+url)
   var chunk = fs.readFile(file_path, function(err, chunk){
-    err? res.statusCode=404 : res.write(chunk)
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+
+    msg = err?'没找到对应文件， 请确认文件名是否正确':chunk
+    res.write(msg,'utf8')
+
     res.end()
   })
 
