afb39dbe31ef4ea1ae829acdc03bda1a409bee35
侯林江 <linjiang.hou@baidao.com>
2016-03-25 11:47:24 +0800

    fix  read css files

--- client+server/server.js
+++ client+server/server.js
@@ -5,7 +5,7 @@ var router = require('./router.js')
 
 http.createServer(function(req, res){
   console.log('request: ', req.url)
-  url = req.url
+  var url = req.url
 
   // 路由到下层
   if(typeof(router[url]) === 'function')
@@ -15,7 +15,9 @@ http.createServer(function(req, res){
   if(url == '/') url = '/index.html'
   file_path = path.join(__dirname+url)
   var chunk = fs.readFile(file_path, function(err, chunk){
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+    //  读文件格式
+    var content_type = 'text/' + url.split('.').pop() + '; charset=utf-8';
+    res.writeHead(200, {'Content-Type': content_type})
 
     msg = err?'没找到对应文件， 请确认文件名是否正确':chunk
     res.write(msg,'utf8')
@@ -24,3 +26,5 @@ http.createServer(function(req, res){
   })
 
 }).listen(9001)
+
+console.log('server start at  http://localhost:9001')
