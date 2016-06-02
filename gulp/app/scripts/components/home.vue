a09e98d3ed9b643ebeaff154553903178d179f6b
侯林江 <linjiang.hou@baidao.com>
2016-06-02 12:40:08 +0800

    vue 动画验证 in gulp/*/home.vue

--- gulp/app/scripts/components/home.vue
+++ gulp/app/scripts/components/home.vue
@@ -1,4 +1,3 @@
-<style lang="stylus"></style>
 <template>
 <h1>home vue</h1>
 <p>asdfasdf</p>
@@ -13,5 +12,64 @@
     </p>
   </div>
 
+  <p>list 1</p>
+  <ul class="content">
+  	<li v-for='num in list' transition="expand" class=#{{num}}>
+  		hello_#{{num}}
+  	</li>
+  </ul>
+
+
+  <p>list 2</p>
+  <ul class="content">
+  	<li v-for='num in list2' transition="expand" class=#{{num}}>
+  		hello_#{{num}}
+  	</li>
+  </ul>
 </div>
 </template>
+
+<script type="text/javascript">
+module.exports = {
+	data: function(){
+		return {
+			count: 1,
+			listWhole: [0,1,2,3,4,5],
+			list: [-1],
+			list2: [0]
+		}
+	},
+
+	ready: function(){
+		var self = this
+		setInterval(function(){
+			self.list[0] = self.listWhole[++self.count%5]
+
+			self.list2.push(self.count)
+			// self.list2.shift()
+			setTimeout(function(){self.list2.shift()}, 50)
+
+		}, 2000)
+	}
+}
+</script>
+
+<style lang="stylus">
+	/* 必需 */
+.expand-transition {
+  transition: all 1s ease;
+  height: 30px;
+  padding: 10px;
+  background-color: #eee;
+  overflow: hidden;
+}
+
+/* .expand-enter 定义进入的开始状态 */
+/* .expand-leave 定义离开的结束状态 */
+.expand-enter, .expand-leave {
+  height: 0;
+  padding: 0 10px;
+  opacity: 0;
+}
+
+</style>
